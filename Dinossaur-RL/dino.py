ec74d67fcf04c473cc4e7448fca27d9e98ce934e
ruandocini <ruandocini@gmail.com>
2020-05-18 23:34:49 -0300

    adding the second project

---
+++ Dinossaur-RL/dino.py
@@ -0,0 +1,8 @@
+#There are 5 key components of a genetic algo,
+# Chromosomes, Generation, Selection, Crossover, Mutation
+# Chromosomes: represent a potential solution to the problem
+# Gereration: comprises multiple chromosomes, we get a new generation after each iteration
+# Selection: based on "fitness", compare two chromosomes and select the better one to pass on the next generation
+# Crossover: process of mating to form a better solution
+# Mutation: Random changes in some solutions to mimic mutation
+
